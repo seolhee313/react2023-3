6f77932fcac1e258911feeced75da34a2bc4f8d2
seolhee313 <125417882+seolhee313@users.noreply.github.com>
2023-05-10 22:49:19 +0900

    집

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+function Hello(props){
+  return (
+    <div>
+      <div>{props.name}</div>
+      <div>{props.text}</div>
+      <div>{props.author}</div>
+      <div>{props.author1}</div>
+    </div>
+  )
+}
+
+const name = {
+  name : "cheonseolhee",
+  text : "hello",
+  author : {
+    name: "너무",
+    name1 : "어려워여",
+  }
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<Hello name={name.name} text={name.text} author={name.author.name} author1={name.author.name1} />);
